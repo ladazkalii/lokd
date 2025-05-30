9ed6c871aa5ad692042e33e6df6fb055bea10c59
ladazkalii <ladazkalii@gmail.com>
2025-05-30 16:53:35 +0700

    Update lok1.py

--- lok1.py
+++ lok1.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/3976e6b8-d48e-4276-b8d3-caebdd44b471")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/c451abe9-9988-4ff8-8ef7-ed16ff7a7278")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
