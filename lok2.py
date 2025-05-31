137fa751cf3f3ce2515bdbc4e4908cba872627bd
ladazkalii <ladazkalii@gmail.com>
2025-05-31 23:19:05 +0700

    Update lok2.py

--- lok2.py
+++ lok2.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://8ab4e23e37d62bc26e35__cr.ar:3efeffa0d3f66c71@gw.dataimpulse.com:12202',
-        'https': 'https://8ab4e23e37d62bc26e35__cr.ar:3efeffa0d3f66c71@gw.dataimpulse.com:12202',
+        'http': 'http://8ab4e23e37d62bc26e35__cr.bg:3efeffa0d3f66c71@gw.dataimpulse.com:10202',
+        'https': 'https://8ab4e23e37d62bc26e35__cr.bg:3efeffa0d3f66c71@gw.dataimpulse.com:10202',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
