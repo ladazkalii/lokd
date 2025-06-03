fc2615db62678baec5bd1fd90e17456c7293b745
ladazkalii <ladazkalii@gmail.com>
2025-06-03 18:30:56 +0700

    Update lok1.py

--- lok1.py
+++ lok1.py
@@ -47,7 +47,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/f2f8fa48-2b3d-4de0-b663-2dea55041cb7")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/2391b952-3a1c-48a0-ba4a-e2d7d42c668c")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
